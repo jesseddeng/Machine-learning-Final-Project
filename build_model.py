5edb500c7a494b5956fe4d0b15c3bcf87cc37289
Jesse Deng <38820434+jesseddeng@users.noreply.github.com>
2018-12-08 16:29:29 -0600

    Add files via upload

---
+++ build_model.py
@@ -0,0 +1,10 @@
+from sklearn.externals import joblib
+import numpy as np
+
+if __name__ == "__main__":
+        
+        
+        prediction = estimator.predict(instance)
+
+print((np.exp(int(prediction[0])  ))
+   
